faa71452e083ca28762184f782571c988dfdea78
赵凯文 <kevin_2233@qq.com>
2021-11-11 15:35:16 +0800

    💄 上传界面

--- src/pages/upload/Upload.vue
+++ src/pages/upload/Upload.vue
@@ -10,6 +10,22 @@
         <p class="description">拖拽文件到这里，或 <a>选择文件</a></p>
         <p class="sub-desc">文件最大支持 20MB</p>
       </div>
+      <ul class="upload-list">
+        <li class="upload-file">
+          <div class="file-container">
+            <div class="file-content">
+              <p class="file-name">图片文件名.jpg</p>
+              <p class="file-desc">
+                <span>68%</span><span>大约还需 12 秒</span>
+              </p>
+            </div>
+            <div class="file-action">
+              <i class="icon close ri-close-line"></i>
+            </div>
+          </div>
+          <div class="file-process"></div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
