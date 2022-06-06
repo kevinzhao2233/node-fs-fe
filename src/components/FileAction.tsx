0ef9658fbc65866188bcb55e752a5cec70d235d9
赵凯文 <kevin_2233@qq.com>
2022-06-06 23:00:22 +0800

    ✨ fileAction 添加状态，可以更丝滑的管理各个上传、接收的状态了，nice

--- src/components/FileAction.tsx
+++ src/components/FileAction.tsx
@@ -6,15 +6,28 @@ import UploadForm from './UploadForm';
 import UploadList from './UploadList';
 import UploadProgress from './UploadProgress';
 
-type Action = 'uploadFile' | 'downloadFile' | 'uploadFolder'
+type Action = 'uploadFile' | 'receiveFile' | 'uploadFolder';
+
+type State =
+'normal' | // 初始状态，等待用户选择上传文件、接收文件等等
+'upload-pending' |
+'uploading' |
+'upload-complate'
 
 function UploadComp() {
   const [action, setAction] = useState<Action>('uploadFile');
+
+  const [state, setState] = useState<State>('uploading');
+
+  const updateState = (_state: State) => {
+    setState(_state);
+  };
+
   return (
     <div className="w-340px m-7 mr-2 mb-15 p-4 rounded-3xl transition-all duration-300 ease-out
     bg-[#FFFFFFa7] shadow-xl shadow-gray-200/40 dark:shadow-gray-800/40 select-none"
     >
-      <UploadList />
+      {state === 'upload-pending' && <UploadList />}
       <div className="flex justify-between items-center px-3">
         <div className="btn grid place-items-center gap-4 w-11 h-11 cursor-pointer border border-gray-500 dark:border-gray-700 rounded-xl">
           <Plus size="20" />
@@ -26,7 +39,7 @@ function UploadComp() {
               action !== 'uploadFolder' ? 'text-xl font-semibold' : 'text-gray-900/80',
             )}
             onMouseEnter={() => { setAction('uploadFile'); }}
-          >添加文件
+          >{state === 'upload-pending' && '继续'}添加文件
           </p>
           <p
             className={cn(
@@ -35,27 +48,34 @@ function UploadComp() {
             )}
             onMouseEnter={() => { setAction('uploadFolder'); }}
             onMouseLeave={() => { setAction('uploadFile'); }}
-          >添加文件夹
+          >{state === 'upload-pending' && '继续'}添加文件夹
           </p>
         </div>
-        <div
-          className="btn grid place-items-center w-100px h-40px cursor-pointer hover:font-semibold
-        bg-white rounded-3xl shadow shadow-[0px_6px_24px_rgba(150,150,150,0.25)]
-        transition-all duration-200"
-          onMouseEnter={() => { setAction('downloadFile'); }}
-        >
-          接收文件
-        </div>
+        {state === 'normal' && (
+          <div
+            className="btn grid place-items-center w-100px h-40px cursor-pointer hover:font-semibold
+              bg-white rounded-3xl shadow shadow-[0px_6px_24px_rgba(150,150,150,0.25)]
+              transition-all duration-200"
+            onMouseEnter={() => { setAction('receiveFile'); }}
+          >
+            接收文件
+          </div>
+        )}
       </div>
-      <UploadForm />
-      <UploadProgress />
-      <div className="flex justify-center pt-6 pb-3">
-        <button
-          className="btn px-12 py-3 bg-blue-600 text-light-50 rounded-14px font-semibold shadow-xl shadow-blue-500/50"
-          type="button"
-        >开始上传
-        </button>
-      </div>
+      {state === 'upload-pending' && (
+        <>
+          <UploadForm />
+          <div className="flex justify-center pt-6 pb-3">
+            <button
+              className="btn px-12 py-3 bg-blue-600 text-light-50 rounded-14px font-semibold shadow-xl shadow-blue-500/50"
+              type="button"
+            >开始上传
+            </button>
+          </div>
+        </>
+      )}
+      {state === 'uploading' && <UploadProgress />}
+      {state === 'uploading' && <UploadList />}
     </div>
   );
 }
--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -15,7 +15,7 @@ function UploadForm() {
         <p>下载需要密码</p>
         <Switch />
       </div>
-      <div className="h-20 px-3 py-2 border-b-1 border-gray-300 relative">
+      <div className="h-20 px-2 py-2 border-gray-300 relative">
         <textarea
           className="p-1 w-full h-full border-gray-400 border-dashed border-1 focus:border-gray-600
             bg-transparent resize-none outline-none rounded-8px"
