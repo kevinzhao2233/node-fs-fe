3527fb72a991ff68550fb1b4ed1659886ccb189b
赵凯文 <kevin_2233@qq.com>
2022-06-14 07:50:03 +0800

    ✨ 实现打开文件和文件夹功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,6 +74,5 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
-
   },
 };
--- src/components/FileAction/BaseAction.tsx
+++ src/components/FileAction/BaseAction.tsx
@@ -1,6 +1,8 @@
 import { Plus } from '@icon-park/react';
 import cn from 'classnames';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { uploadChunkFile } from '@/api';
 
 import type { State } from './FileAction';
 
@@ -15,33 +17,54 @@ interface P {
 function BaseAction({ state, updateState, clearPreUpload }: P) {
   const [action, setAction] = useState<Action>('uploadFile');
 
+  const inputFolderRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputFolderRef.current) {
+      inputFolderRef.current.setAttribute('directory', '');
+      inputFolderRef.current.setAttribute('webkitdirectory', '');
+    }
+  }, [inputFolderRef]);
+
+  const chooseFile = () => {
+
+  };
+
+  const chooseFolder = () => {
+  };
+
   return (
     <div className="flex justify-between items-center px-3">
-      <div
-        className="btn grid place-items-center gap-4 w-11 h-11 cursor-pointer border border-gray-500 dark:border-gray-700 rounded-xl
+      <label
+        htmlFor="inputId"
+        className="block btn grid place-items-center gap-4 w-11 h-11 cursor-pointer border border-gray-500 dark:border-gray-700 rounded-xl
           hover:(text-indigo-600)"
       >
         <Plus size="20" />
-      </div>
+      </label>
       <div className="mr-auto ml-3">
-        <p
+        <label
+          htmlFor="inputId"
           className={cn(
-            'btn cursor-pointer transition-all duration-300 ease-out leading-none mb-1',
+            'block btn cursor-pointer transition-all duration-300 ease-out leading-none mb-1',
             action !== 'uploadFolder' ? 'text-xl font-semibold' : '',
           )}
           onMouseEnter={() => { setAction('uploadFile'); }}
         >{state === 'uploadPending' && '继续'}添加文件
-        </p>
-        <p
+        </label>
+        <label
+          htmlFor="inputFolderId"
           className={cn(
-            'btn cursor-pointer transition-all duration-300 ease-out leading-none',
+            'block btn cursor-pointer transition-all duration-300 ease-out leading-none',
             action === 'uploadFolder' ? 'text-xl font-semibold' : '',
           )}
           onMouseEnter={() => { setAction('uploadFolder'); }}
           onMouseLeave={() => { setAction('uploadFile'); }}
         >或添加文件夹
-        </p>
+        </label>
       </div>
+      <input type="file" id="inputId" multiple onChange={chooseFile} style={{ display: 'none' }} />
+      <input ref={inputFolderRef} type="file" id="inputFolderId" multiple onChange={chooseFolder} style={{ display: 'none' }} />
       {(state === 'normal' || state === 'uploadPending') && (
         <div
           className="btn grid place-items-center w-100px h-40px cursor-pointer hover:font-semibold
