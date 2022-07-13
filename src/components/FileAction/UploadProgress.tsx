58a1664e465880b7eabe03318ed3bbea8a052e1f
赵凯文 <kevin_2233@qq.com>
2022-07-13 21:16:08 +0800

    💄 调整上传进度 UI

--- src/components/FileAction/UploadProgress.tsx
+++ src/components/FileAction/UploadProgress.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 function UploadProgress() {
   return (
     <div className="my-4 mb-8 px-3">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-1 text-14px">
         <span>96MB / 478 MB | 20%</span>
         <span>还需 37S</span>
       </div>
       <div className="h-2 bg-blue-gray-500/10 rounded-4px overflow-hidden">
-        <div className="w-[20%] h-full bg-gradient-to-r from-green-500 to-blue-500 rounded-4px" />
+        <div className="w-[20%] h-full bg-gradient-to-r from-blue-500 to-indigo-500 rounded-4px" />
       </div>
     </div>
   );
